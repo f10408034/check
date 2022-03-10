2fd0a759174f362663eb264fc3ece121f11f08ab
lm <f10408034@gmail.com>
2022-03-10 18:04:40 +0800

    UI commit

--- app/src/main/java/com/vangood/chat/MainActivity.kt
+++ app/src/main/java/com/vangood/chat/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.vangood.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.vangood.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+    lateinit var binding: ActivityMainBinding
+    val personResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){ result ->
+        Log.d(TAG, "back from LoginActivity with data? ")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //botton funtion
+        setupFunctions()
+    }
+
+    private fun setupFunctions() {
+        binding.bPerson.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.bSearch.setOnClickListener {
+
+        }
+        binding.bHome.setOnClickListener {
+
+        }
     }
 }
